e391748137548074b238c8248eadcff6c33f31b4
quarkusio <no-reply@quarkus.io>
2022-01-27 15:57:46 +0000

    Initial commit

---
+++ src/test/java/sf/district09/gent/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package sf.district09.gent;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
